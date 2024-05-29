469ffaace2a02ce587f9dc44591d06736254ee47
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-05-29 19:01:53 +0200

    creating DOM elements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// Banker APP
 
 // Data
 const account1 = {
@@ -61,11 +61,22 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-// LECTURES
-const currencies = new Map([
-  ["USD", "United States Dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound Sterling"],
-]);
+// Creating DOM Elements
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+   </div>
+  `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
