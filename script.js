a49cbe573615794df6f1aa394d8a32da6cfaa010
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-06-01 00:16:53 +0200

    implemented the delete account feature

--- script.js
+++ script.js
@@ -162,6 +162,27 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
+// Delete the account
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    // Deletee account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
+
 // Implementing transfers
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
